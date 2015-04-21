54a4efa05aa613e1d7b349ba71f4fb85ce9eb956
Arjan van Leeuwen <avleeuwen@piwebs.com>
2015-04-21 22:15:55 +0100

    Fast version

--- cpp/optimization_tasks.cpp
+++ cpp/optimization_tasks.cpp
@@ -3,73 +3,102 @@
 #include <vector>
 #include <fstream>
 
-int CalculateSecretNumber(const std::string& content) {
+std::vector<int> FindDigits(std::string content) {
   std::vector<int> list;
-
-  size_t foundpos;
-  std::string haystack = content;
-  while ((foundpos = haystack.find_first_of(
-      std::string("0123456789otfsenOTFSEN"))) != std::string::npos) {
-    haystack = haystack.substr(foundpos);
-
-    if (haystack[0] >= '0' && haystack[0] <= '9') {
-      size_t newpos;
-      list.push_back(std::stoi(haystack, &newpos));
-      haystack = haystack.substr(newpos);
-    } else {
-      if (haystack.substr(0, 3) == "one" ||
-          haystack.substr(0, 3) == "One") {
-        list.push_back(1);
-        haystack = haystack.substr(3);
-      } else if (haystack.substr(0, 3) == "two" ||
-                 haystack.substr(0, 3) == "Two") {
-        list.push_back(2);
-        haystack = haystack.substr(3);
-      } else if (haystack.substr(0, 5) == "three" ||
-                 haystack.substr(0, 5) == "Three") {
-        list.push_back(3);
-        haystack = haystack.substr(5);
-      } else if (haystack.substr(0, 4) == "four" ||
-                 haystack.substr(0, 4) == "Four") {
-        list.push_back(4);
-        haystack = haystack.substr(4);
-      } else if (haystack.substr(0, 4) == "five" ||
-                 haystack.substr(0, 4) == "Five") {
-        list.push_back(5);
-        haystack = haystack.substr(4);
-      } else if (haystack.substr(0, 3) == "six" ||
-                 haystack.substr(0, 3) == "Six") {
-        list.push_back(6);
-        haystack = haystack.substr(3);
-      } else if (haystack.substr(0, 5) == "seven" ||
-                 haystack.substr(0, 5) == "Seven") {
-        list.push_back(7);
-        haystack = haystack.substr(5);
-      } else if (haystack.substr(0, 5) == "eight" ||
-                 haystack.substr(0, 5) == "Eight") {
-        list.push_back(8);
-        haystack = haystack.substr(5);
-      } else if (haystack.substr(0, 4) == "nine" ||
-                 haystack.substr(0, 4) == "Nine") {
-        list.push_back(9);
-        haystack = haystack.substr(4);
-      } else if (haystack.substr(0, 3) == "ten" ||
-                 haystack.substr(0, 3) == "Ten") {
-        list.push_back(10);
-        haystack = haystack.substr(3);
-      } else if (haystack.substr(0, 6) == "eleven" ||
-                 haystack.substr(0, 6) == "Eleven") {
-        list.push_back(11);
-        haystack = haystack.substr(6);
-      } else if (haystack.substr(0, 6) == "twelve" ||
-                 haystack.substr(0, 6) == "Twelve") {
-        list.push_back(12);
-        haystack = haystack.substr(6);
-      } else {
-        haystack = haystack.substr(1);
-      }
-    }
+  for (auto pos = content.find_first_of("0123456789"); pos != std::string::npos;
+       pos = content.find_first_of("0123456789")) {
+    content = content.substr(pos);
+    size_t newpos;
+    list.push_back(std::stoi(content, &newpos));
+    content = content.substr(newpos);
   }
+
+  return list;
+}
+
+unsigned FindWrittenNumberOccurrences(
+    std::string content, const std::string& to_search) {
+  unsigned count = 0;
+
+  for (auto pos = content.find(to_search); pos != std::string::npos;
+       pos = content.find(to_search)) {
+    ++count;
+    content = content.substr(pos + to_search.length());
+  }
+
+  return count;
+}
+
+std::vector<int> AccumulateNumbers(
+    const std::string& content, const std::string& written, int number) {
+  std::vector<int> list;
+  unsigned count = FindWrittenNumberOccurrences(content, written);
+  for (unsigned i = 0; i < count; ++i)
+    list.push_back(number);
+
+  return list;
+}
+
+std::vector<int> FindAllNumbers(const std::string& content) {
+  std::vector<int> list;
+  std::vector<int> result;
+
+  list = AccumulateNumbers(content, "one", 1);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "One", 1);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "two", 2);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Two", 2);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "three", 3);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Three", 3);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "four", 4);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Four", 4);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "five", 5);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Five", 5);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "six", 6);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Six", 6);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "seven", 7);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Seven", 7);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "eight", 8);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Eight", 8);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "nine", 9);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Nine", 9);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "ten", 10);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Ten", 10);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "eleven", 11);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Eleven", 11);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "twelve", 12);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = AccumulateNumbers(content, "Twelve", 12);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+  list = FindDigits(content);
+  std::copy(list.begin(), list.end(), std::back_inserter(result));
+
+  return result;
+}
+
+int CalculateSecretNumber(const std::string& content) {
+  std::vector<int> list = FindAllNumbers(content);
 
   std::sort(list.begin(), list.end());
 
